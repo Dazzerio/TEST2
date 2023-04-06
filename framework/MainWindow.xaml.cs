b1fcb1ad4739fe33fcedae4e021244fef1968c3e
Kostya <kosta3suxar@mail.ru>
2023-04-06 07:52:07 +0600

    popa

---
+++ framework/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace framework
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            baseEntities1 baseEntities = new baseEntities1();
+
+            datagrid.ItemsSource = baseEntities.articles.ToList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            item item = new item();
+            if (item.ShowDialog() == true)
+            {
+                baseEntities1 baseEntities = new baseEntities1();
+                datagrid.ItemsSource = baseEntities.articles.ToList();
+            }
+
+        }
+
+        private void editBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var item_form = (article)datagrid.SelectedItem;
+
+            item item = new item(item_form);
+            if(item.ShowDialog() == true)
+            {
+                baseEntities1 baseEntities = new baseEntities1();
+                datagrid.ItemsSource = baseEntities.articles.ToList();
+            }
+        }
+
+        private void removeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var item = ((article)datagrid.SelectedItem);
+            
+
+
+            baseEntities1 baseEntities = new baseEntities1();
+
+            var it = baseEntities.articles.Where(u=>u.id == item.id).FirstOrDefault();
+
+            baseEntities.articles.Remove(it);
+            baseEntities.SaveChanges();
+
+            datagrid.ItemsSource = baseEntities.articles.ToList();
+        }
+    }
+}
---
+++ framework/item.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace framework
+{
+    /// <summary>
+    /// Логика взаимодействия для item.xaml
+    /// </summary>
+    public partial class item : Window
+    {
+        article article;
+
+        public item(article article1)
+        {
+            InitializeComponent();
+
+            this.article = article1;
+
+            textboxName.Text = article.name;
+            textboxText.Text = article.text;
+        }
+
+        public item()
+        {
+            InitializeComponent();
+
+            this.article = new article();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+
+            baseEntities1 baseEntities = new baseEntities1();
+
+            String name = textboxName.Text.ToString();
+            String text = textboxText.Text.ToString();
+
+            if(name != "" && text != "")
+            {
+                article.text = text;
+                article.name = name;
+                
+                if(article.id_user == 0)
+                    article.id_user = 1;
+
+                if(article.id == 0)
+                {
+                    baseEntities.articles.Add(article);
+                    baseEntities.SaveChanges();
+                } else
+                {
+                    var art = baseEntities.articles.Where(u => u.id == article.id).FirstOrDefault();
+                    art.text = text;
+                    art.name = name;
+                    baseEntities.SaveChanges();
+                }
+
+                
+                DialogResult = true;
+            }
+            else { MessageBox.Show("Заполните поля"); return; }
+        }
+    }
+}
